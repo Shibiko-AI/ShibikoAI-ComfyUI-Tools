498a56921d29fdb828b21d07f7b276596d540165
Brian Jemilo II <jemiloii@gmail.com>
2024-06-26 20:27:52 -0500

    Mask Working

---
+++ nodes/cascades.py
@@ -0,0 +1,148 @@
+import cv2
+import numpy as np
+import os
+import torch
+from typing import Optional
+from ..utils.directory import initialize_directory
+
+
+class Cascade:
+    cascades_directory = initialize_directory('cascades')
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        cascades = [cascade.split(".")[0] for cascade in os.listdir(cls.cascades_directory) if cascade.endswith(".xml")]
+        embedded = [cascade.split(".")[0] for cascade in os.listdir(cv2.data.haarcascades) if cascade.endswith(".xml")]
+
+        cascades.extend(embedded)
+        cascades.sort()
+
+        return {
+            "required": {
+                "image": ("IMAGE",),
+            },
+            "optional": {
+                "cascade": (cascades, {"default": 'frontalface_default'}),
+                "blur": ("INT", {"default": 25, "min": 0, "max": 100, "step": 1},),
+                "blur_type": (["gaussian", "median", "box"], {"default": "gaussian"}),
+                "padding": ("INT", {"default": 50, "min": 0, "step": 1},),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID"
+            },
+        }
+
+    CATEGORY = "Shibiko"
+
+    RETURN_TYPES = ("IMAGE", "MASK", "LIST")
+    RETURN_NAMES = ("IMAGE", "MASK", "LIST")
+
+    FUNCTION = "__call__"
+
+    def __init__(self, cascade='frontalface_default', padding=25):
+        self.cascades_directory = None
+        self.cascade = cascade
+        self.bbox = None
+        self.image = None
+        self.haar_cascade_face = None
+        self.padding = padding
+
+        self.load(cascade)
+
+    def load(self, cascade='frontalface_default'):
+        self.cascade = cascade
+        self.cascades_directory = initialize_directory('cascades')
+
+        if os.path.exists(self.cascades_directory):
+            cascade_path = os.path.join(self.cascades_directory, f'{cascade}.xml')
+            self.haar_cascade_face = cv2.CascadeClassifier(cascade_path)
+        elif cv2.data.haarcascades not in cascade and 'haarcascade_' not in cascade:
+            cascade_path = os.path.join(cv2.data.haarcascades, f'haarcascade_{cascade}.xml')
+            self.haar_cascade_face = cv2.CascadeClassifier(cascade_path)
+        else:
+            raise ValueError('Invalid cascade path or name')
+
+    def detect(self, image):
+        self.image = image
+
+        # Check Frame Color
+        if len(self.image.shape) > 2:
+            gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        else:
+            gray = self.image
+
+        # Detect faces in the image
+        self.bbox = self.haar_cascade_face.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(24, 24))
+        return self.bbox
+
+    def draw(self, image=None, padding=None):
+        if image is not None:
+            self.image = image.copy()
+
+        if padding is None:
+            padding = self.padding
+
+        # Draw rectangles around the faces
+        for (x, y, w, h) in self.bbox:
+            cv2.rectangle(self.image, (x - padding, y - padding), (x + w + padding, y + h + padding), (255, 0, 0), 2)
+
+        return self.image
+
+    def mask(self, blur=25, blur_type='gaussian', padding: Optional[int] = None):
+        if self.bbox is None or self.image is None:
+            raise ValueError('Cascades or image not detected')
+
+        if padding is not None:
+            self.padding = padding
+
+        mask = self.image.copy()
+        mask.fill(0)
+
+        # Whiteout the detected faces on the mask
+        for (x, y, w, h) in self.bbox:
+            mask[y - self.padding:y + h + self.padding, x - self.padding:x + w + self.padding] = 255
+
+        if blur is not None:
+            if blur_type == 'gaussian':
+                mask = cv2.GaussianBlur(mask, (blur, blur), 0)
+            elif blur_type == 'median':
+                mask = cv2.medianBlur(mask, blur)
+            elif blur_type == 'box':
+                mask = cv2.blur(mask, (blur, blur))
+            else:
+                pass
+
+        return self.CV2_to_NHWC(mask, cv2.COLOR_BGR2GRAY)
+
+    @staticmethod
+    def NHWC_to_CV2(image):
+        image_numpy = image.squeeze().cpu().numpy()
+        image_bgr = image_numpy[:, :, [2, 1, 0]]
+        image_bgr = (image_bgr * 255).astype(np.uint8)
+
+        return image_bgr
+
+    @staticmethod
+    def CV2_to_NHWC(image, code=cv2.COLOR_BGR2RGB):
+        image_rgb = cv2.cvtColor(image, code)
+        image_rgb = image_rgb.astype(np.float32) / 255.0
+        image_rgb = image_rgb[np.newaxis, :]
+        image_tensor = torch.from_numpy(image_rgb)
+
+        return image_tensor
+
+    def __call__(self, image, blur=25, blur_type='gaussian', cascade='frontalface_default', padding=50, unique_id=None):
+        if cascade != self.cascade:
+            self.load(cascade)
+
+        image = self.NHWC_to_CV2(image)
+        self.detect(image)
+        self.draw(image, padding)
+        mask = self.mask(blur, blur_type, padding)
+
+        return self.CV2_to_NHWC(self.image), mask, self.bbox
+
+
+NODE_CLASS_MAPPINGS = {"Cascade": Cascade}
+NODE_DISPLAY_NAME_MAPPINGS = {"Cascade": "Shibiko AI - Cascade Detection"}
+
