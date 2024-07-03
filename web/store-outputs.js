b9dcf15e6109e83178e59c3c2fc2dc345f658ec0
Brian Jemilo II <jemiloii@gmail.com>
2024-07-03 05:26:37 -0500

    Remove Show Code as I ported it to AnyNode

--- web/store-outputs.js
+++
@@ -1,8 +0,0 @@
-export function storeAnyNodeOutputs(node) {
-  node.onExecuted = function (event) {
-    node.outputs_values = node.outputs_values || [];
-    node.outputs.forEach((output, index) => {
-      node.outputs_values[index] = event[output.name] && event[output.name][0] || undefined;
-    });
-  };
-}
